d1c227b6bb3c8b98581565812cc7d542e187521e
thuan <dinhthuan2020@gmail.com>
2020-01-08 11:16:20 +0700

    Initial Commit

---
+++ MyFirstApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApplication
+//
+//  Created by Catalina on 1/8/20.
+//  Copyright © 2020 Catalina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
